ae77b8885131dc789b5931ee6aee7f93d10b3ecb
davidsamuel23 <91531206+davidsamuel23@users.noreply.github.com>
2021-12-01 12:52:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot() {
 
 console.log("ml5 version:",ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zY57a0Plj/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Qorn6llzx/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded Successfully!");
@@ -30,4 +30,49 @@ function speak() {
     var speak_data = "The Prediction Is "+prediction;
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+}
+
+function check() {
+    img = document.getElementById("image_captured");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+        if(results[0].label == "Amazing"){
+            document.getElementById("result_emoji").innerHTML = "&#128076;";
+            document.getElementById("quote").innerHTML = "Oh! Thank You For Saying The Food Is Amazing";
+        }
+        if(results[0].label == "Best"){
+            document.getElementById("result_emoji").innerHTML = "&#128077";
+            document.getElementById("quote").innerHTML = "Thank You! I Am Going To Attend The Exam Today";
+        }
+        if(results[0].label == "Victory"){
+            document.getElementById("result_emoji").innerHTML = "&#9996;";
+            document.getElementById("quote").innerHTML = "Oh! You Won The Drawing Competition Congratulations";
+        }
+        if(results[0].label == "Unity"){
+            document.getElementById("result_emoji").innerHTML = "&#9994;";
+            document.getElementById("quote").innerHTML = "No No No Don't Hit Me As You Have Power";
+        }
+        if(results[0].label == "Clap"){
+            document.getElementById("result_emoji").innerHTML = "&#128079;";
+            document.getElementById("quote").innerHTML = "No No No I Should Give You An Applause";
+        }
+        if(results[0].label == "Rock"){
+            document.getElementById("result_emoji").innerHTML = "&#129304;";
+            document.getElementById("quote").innerHTML = "Oh! You Are On A Party Enjoy It";
+        }
+        if(results[0].label == "Bye"){
+            document.getElementById("result_emoji").innerHTML = "&#128075;";
+            document.getElementById("quote").innerHTML = "Same To You Bye Bye";
+        }
+    }
 }
